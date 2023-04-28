aa368a0095cbef0ea9b204b134ff9a3c15699ebd
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-28 10:16:15 +0200

    MILESTONE1 - added attributes, getters, setters to Event class

--- GestoreEventi/Event.cs
+++ GestoreEventi/Event.cs
@@ -8,5 +8,30 @@ namespace GestoreEventi
 {
     public class Event
     {
+        //ATTRIBUTES
+        private string title;
+        private DateTime eventDate;
+        private int maximumSeats;
+        private int numberOfReservations;
+
+        //CONSTRUCTOR
+
+        //GETTERS
+        public string GetTitle() { return title; }
+     
+        public DateTime GetEventDate() { return eventDate; } 
+        
+        public int GetMaximumSeats() { return maximumSeats; }
+
+        public int GetNumberOfReservations() {  return numberOfReservations; }
+
+        //SETTERS
+
+        public void SetTitle(string title) { this.title = title;}
+
+        public void SetEventDate(DateTime eventDate) {  this.eventDate = eventDate;}
+
+        //METHODS
+
     }
 }
