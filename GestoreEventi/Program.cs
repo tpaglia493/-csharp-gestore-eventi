bdb7559173f943a4aeb34a53f814db238bee5052
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-28 11:31:46 +0200

    MILESTONE2 - added event constructor in main program

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,17 +1,20 @@
-﻿Console.WriteLine("Welcome to your EventPlanner");
+﻿using GestoreEventi;
+
+Console.WriteLine("Welcome to your EventPlanner");
 
 //USER INPUTS TO GET ATTRIBUTES FOR THE EVENT CONSTRUCTOR
 Console.Write("Please insert the title of the event you wish to plan: ");
 string userInputEventTitle = Console.ReadLine();
 
 Console.Write("Please insert the date of the event in format dd/mm/yyyy: ");
-string userInputEventDate = Console.ReadLine();
+DateTime userInputEventDate = DateTime.Parse(Console.ReadLine());
 
 Console.Write("Please insert the number of maximum seats for the event: ");
 int userInputMaximumSeats = int.Parse(Console.ReadLine());
 
 //EVENT CONSTRUCTION
 
+Event newEvent = new Event(userInputEventTitle, userInputEventDate, userInputMaximumSeats);
 
 Console.WriteLine("How many seats would you like to reserve?");
 int userInputSeatsToReserve =  int.Parse(Console.ReadLine());
