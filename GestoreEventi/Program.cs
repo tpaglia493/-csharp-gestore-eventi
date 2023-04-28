3719d99affc58e17adcbc16447c6968f94dde71b
tpaglia493 <tommasopaglia93@outlook.it>
2023-04-28 12:30:53 +0200

    MILESTONE2 - deleted while cicle to get reservations - added while cicle to delete reservations as long as user wants

--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -18,19 +18,38 @@ Event newEvent = new Event(userInputEventTitle, userInputEventDate, userInputMax
 
 
 //USER INTERFACE
-bool userWantsToReserve = true;
 
-while (userWantsToReserve) 
-{ 
-    Console.WriteLine($"Would you like to reserve seats for the event '{newEvent.GetTitle()}'?");
-    string userAnswer = Console.ReadLine().ToUpper();
-    Console.WriteLine(userAnswer);
-    if (userAnswer != "SI" && userAnswer != "YES" && userAnswer != "Y" && userAnswer != "SÌ" && userAnswer != "S")
-    {
-        userWantsToReserve = false;
-    }
-Console.WriteLine("How many seats would you like to reserve?");
-int userInputSeatsToReserve =  int.Parse(Console.ReadLine());
+//SEATS RESERVATION
+Console.WriteLine($"Would you like to reserve seats for the event '{newEvent.GetTitle()}'?");
+string userWantsToReserve = Console.ReadLine().ToUpper();
+if (userWantsToReserve == "SI" || userWantsToReserve == "YES" || userWantsToReserve == "Y" || userWantsToReserve == "SÌ" || userWantsToReserve == "S")
+{
+    Console.WriteLine("How many seats would you like to reserve?");
+    int userInputSeatsToReserve =  int.Parse(Console.ReadLine());
+    newEvent.reserveSeats(userInputSeatsToReserve);
 }
-Console.WriteLine(newEvent.ToString());
+
+Console.WriteLine($"\nThe maximum capacity for the event is of {newEvent.GetMaximumSeats()} seats");
+Console.WriteLine($"The number of actual reservations is {newEvent.GetNumberOfReservations()}");
+Console.WriteLine($"There are now {newEvent.GetMaximumSeats() - newEvent.GetNumberOfReservations()} remaining seats");
+
+
+
+//CANCEL RESERVATIONS
+bool userWantsToCancel = true;
+while (userWantsToCancel)
+{
+    Console.WriteLine($"Would you like to cancel reservations for the event '{newEvent.GetTitle()}'?");
+    string userAnswer = Console.ReadLine().ToUpper();
+    if (userAnswer == "SI" || userAnswer == "YES" || userAnswer == "Y" || userAnswer == "SÌ" || userAnswer == "S")
+    {
+        Console.WriteLine("How many reservations would you like to cancel?");
+        int userInputReservationsToCancel = int.Parse(Console.ReadLine());
+        newEvent.cancelReservations(userInputReservationsToCancel);
+        Console.WriteLine($"The number of actual reservations is {newEvent.GetNumberOfReservations()}");
+        Console.WriteLine($"There are now {newEvent.GetMaximumSeats() - newEvent.GetNumberOfReservations()} remaining seats");
+    }
+    else { userWantsToCancel = false; }
+
+}
 
